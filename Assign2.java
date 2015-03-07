6dbaa9f8aad43fe8e8dd16c6378ed3fccf5cb81f
serglou91 <sergiolourenco@live.com>
2015-03-07 13:17:42 -0800

    Comments and inputs ran

--- Assign2.java
+++ Assign2.java
@@ -1,16 +1,34 @@
-/**
- * @(#)Assign2.java
- *
- *
- * @author
- * @version 1.00 2015/3/6
- */
+//--------------------------------------------------------------------
+//Sergio Lourenco T00011397
+//COMP 3050
+//Assign2.java
+//The purpose of this program is to demonstrate how to calculate
+//factorials and fibonacci terms both recursively and non-recursively.
+//Language: Java
+//Editor: Eclipse
+//Compiler: Eclipse Compiler for Java
+//OS: Windows 8.1 (64-bit)
+//Computer Used: Personal Desktop Computer
+//--------------------------------------------------------------------
 
 
 public class Assign2 {
 
     public static void main(String [] args){
-    	/*System.out.println("0! = " + FactP2(0));
+    	
+    	System.out.println("0! = " + FactP1(0));
+    	System.out.println("1! = " + FactP1(1));
+    	System.out.println("2! = " + FactP1(2));
+    	System.out.println("3! = " + FactP1(3));
+    	System.out.println("4! = " + FactP1(4));
+    	System.out.println("5! = " + FactP1(5));
+    	System.out.println("6! = " + FactP1(6));
+    	System.out.println("7! = " + FactP1(7));
+    	System.out.println("8! = " + FactP1(8));
+    	System.out.println("9! = " + FactP1(9));
+    	System.out.println("10! = " + FactP1(10));
+    	
+    	System.out.println("0! = " + FactP2(0));
     	System.out.println("1! = " + FactP2(1));
     	System.out.println("2! = " + FactP2(2));
     	System.out.println("3! = " + FactP2(3));
@@ -21,19 +39,32 @@ public class Assign2 {
     	System.out.println("8! = " + FactP2(8));
     	System.out.println("9! = " + FactP2(9));
     	System.out.println("10! = " + FactP2(10));
-    	*/
+    	
 
-    	System.out.println(FibonP2(0));
-    	System.out.println(FibonP2(1));
-    	System.out.println(FibonP2(2));
-    	System.out.println(FibonP2(3));
-    	System.out.println(FibonP2(4));
-    	System.out.println(FibonP2(5));
-    	System.out.println(FibonP2(6));
-    	System.out.println(FibonP2(7));
-    	System.out.println(FibonP2(8));
-    	System.out.println(FibonP2(9));
-    	System.out.println(FibonP2(10));
+    	System.out.println("F0= " +FibonP1(0));
+    	System.out.println("F1= " +FibonP1(1));
+    	System.out.println("F2= " +FibonP1(2));
+    	System.out.println("F3= " +FibonP1(3));
+    	System.out.println("F4= " +FibonP1(4));
+    	System.out.println("F5= " +FibonP1(5));
+    	System.out.println("F6= " +FibonP1(6));
+    	System.out.println("F7= " +FibonP1(7));
+    	System.out.println("F8= " +FibonP1(8));
+    	System.out.println("F9= " +FibonP1(9));
+    	System.out.println("F10= " +FibonP1(10));
+    	
+    	System.out.println("F0= " +FibonP2(0));
+    	System.out.println("F1= " +FibonP2(1));
+    	System.out.println("F2= " +FibonP2(2));
+    	System.out.println("F3= " +FibonP2(3));
+    	System.out.println("F4= " +FibonP2(4));
+    	System.out.println("F5= " +FibonP2(5));
+    	System.out.println("F6= " +FibonP2(6));
+    	System.out.println("F7= " +FibonP2(7));
+    	System.out.println("F8= " +FibonP2(8));
+    	System.out.println("F9= " +FibonP2(9));
+    	System.out.println("F10= " +FibonP2(10));
+    	
     }
 
     public static int FactP1(int n){
